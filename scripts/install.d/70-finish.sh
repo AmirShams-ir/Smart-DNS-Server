8de2b51cb6054fc35b5544db2b8b9e5dec54f1da
Amir Shams <amirshams.ir@gmail.com>
2026-07-12 05:14:02 +0330

    finish

--- scripts/install.d/70-finish.sh
+++ scripts/install.d/70-finish.sh
@@ -8,8 +8,16 @@ if systemctl is-active --quiet systemd-resolved; then
     systemctl stop --now systemd-resolved
     systemctl disable --now systemd-resolved
 fi
-
 sleep 1
+
+if [ -L /etc/resolv.conf ]; then
+    rm -f /etc/resolv.conf
+fi
+
+cat >/etc/resolv.conf <<EOF
+nameserver 127.0.0.1
+options edns0 trust-ad
+EOF
 
 info "Testing configuration"
 
